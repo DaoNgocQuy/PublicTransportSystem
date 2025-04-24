799ab97d9b5393556062bf3e1dabef1c7bda45cc
DaoNgocQuy <139777419+DaoNgocQuy@users.noreply.github.com>
2025-04-24 12:13:50 +0700

    chay web

--- src/main/java/com/pts/configs/DispatcherServletInit.java
+++ src/main/java/com/pts/configs/DispatcherServletInit.java
@@ -1,30 +1,27 @@
-///*
-// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
-// */
-//package com.pts.configs;
-//
-//import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-//
-///**
-// *
-// * @author 84353
-// */
-//public class DispatcherServletInit extends AbstractAnnotationConfigDispatcherServletInitializer{
-//
-//    @Override
-//    protected Class<?>[] getRootConfigClasses() {
-//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-//    }
-//
-//    @Override
-//    protected Class<?>[] getServletConfigClasses() {
-//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-//    }
-//
-//    @Override
-//    protected String[] getServletMappings() {
-//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-//    }
-//    
-//}
+package com.pts.configs;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{
+            SpringSecurityConfigs.class,
+            HibernateConfigs.class // nếu không có thì có thể bỏ đi
+        };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{
+            WebAppContextConfigs.class,
+            ThymeleafConfig.class // nếu bạn có cấu hình Thymeleaf
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
--- src/main/java/com/pts/configs/SecurityWebApplicationInitializer.java
+++ src/main/java/com/pts/configs/SecurityWebApplicationInitializer.java
@@ -2,14 +2,14 @@
 // * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
 // * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
 // */
-//package com.pts.configs;
-//
-//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-//
-///**
-// *
-// * @author admin
-// */
-//public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-//    
-//}
+package com.pts.configs;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ *
+ * @author admin
+ */
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+    
+}

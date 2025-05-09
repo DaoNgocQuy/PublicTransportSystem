de8a43b613603b7926c64a1787476dbb351e92bf
Huy <dnmhuy@gmail.com>
2025-05-10 01:59:15 +0700

    commit

--- src/main/java/com/pts/controllers/AuthController.java
+++ src/main/java/com/pts/controllers/AuthController.java
@@ -1,31 +1,10 @@
 package com.pts.controllers;
 
-import com.pts.pojo.Users;
-import com.pts.services.CloudinaryService;
-import com.pts.services.UserService;
-import java.io.IOException;
-import java.util.Date;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class AuthController {
-
-    @Autowired
-    private UserService userService;
-    
-    @Autowired
-    private CloudinaryService cloudinaryService;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-    
     @GetMapping("/login")
     public String login() {
         return "login";
